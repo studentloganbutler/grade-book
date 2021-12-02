f6d451cbaa68bf0c46f3808c0bf7690cf733e48c
Logan Butler <logan.butler@student.swic.edu>
2021-12-01 19:38:39 -0600

    Correct import for config

--- app/client.js
+++ app/client.js
@@ -1,8 +1,8 @@
 // Loads a reusable Mongo client for the application.
 import { MongoClient } from "mongodb";
-import config from "./config";
+import config from "./config.js";
 
-const client = new MongoClient(config.db.clientURL);
+const client = new MongoClient(config.db.url);
 
 client
   .connect()
