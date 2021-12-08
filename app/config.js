02261231165fcb9405d5e182d40a2de2365fd231
Logan Butler <logan.butler@student.swic.edu>
2021-12-08 14:07:01 -0600

    Add saltRounds for encryption

--- app/config.js
+++ app/config.js
@@ -8,5 +8,9 @@ export default {
     name: "gradeBookDatabase",
     url: process.env.MONGO_URI,
   },
+  encryption: {
+    saltRounds: process.env.SALT_ROUNDS || 10,
+    secret: process.env.ENCRYPTION_SECRET || "secret",
+  },
   port: process.env.PORT || 3000,
 };
