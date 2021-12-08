fb90369dc869e0d07e69834e73b20cf5027d36d5
Logan Butler <logan.butler@student.swic.edu>
2021-12-07 20:57:27 -0600

    Add and Edit register route

--- app/Routes/admin.js
+++ app/Routes/admin.js
@@ -7,10 +7,14 @@ router.get("/", (_, res) => {
   res.send("Hello there from Admin!");
 });
 
-router.post("/register", (req, res) => {
-  const { username, password } = req.body;
-  adminController.create(username, password);
-  res.send("Post request to register");
+router.post("/register", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    adminController.create(username, password);
+    res.send("Post request to register");
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
 });
 
 export default router;
