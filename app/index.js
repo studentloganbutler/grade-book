4168668873aefaec0e7050626a53e0db005fa035
Logan Butler <logan.butler@student.swic.edu>
2021-12-08 20:25:02 -0600

    Add isAuth to app.use, reorganize

--- app/index.js
+++ app/index.js
@@ -1,15 +1,17 @@
 import express from "express";
 import config from "./config.js";
+import isAuth from "./middleware/isAuth.js";
 import router from "./routes/index.js";
 
 const app = express();
 
-app.use(express.json());
-app.use("/api", router);
-
 app.get("/", (_, res) => {
   res.send("Hello World");
 });
+
+app.use(express.json());
+app.use(isAuth);
+app.use("/api", router);
 
 app.listen(config.port, () => {
   console.log("Server running on port 3000");
