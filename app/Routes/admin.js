2d054a5e1cf9eaea9ec3cd594278117d6595604a
Logan Butler <logan.butler@student.swic.edu>
2021-12-14 17:11:53 -0600

    Add status type to error, edit login route

--- app/Routes/admin.js
+++ app/Routes/admin.js
@@ -24,7 +24,17 @@ router.post("/register", async (req, res) => {
 
     const token = await adminController.show(admin);
 
-    res.send(token);
+    res.status(201).send(token);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    const token = await adminController.show(req.body);
+
+    res.status(200).json({ token });
   } catch (err) {
     res.status(400).json({ message: err.message });
   }
