c1a7876cee2510e2609fb5673fdee9795b7803f6
Logan Butler <logan.butler@student.swic.edu>
2021-12-14 19:50:54 -0600

    Remove async await from default export

--- app/Controllers/assignments.js
+++ app/Controllers/assignments.js
@@ -4,8 +4,7 @@ import config from "../config.js";
 const assignments = client.db(config.db.name).collection("assignments");
 
 export default {
-  async create(newAssignment) {
-    const result = await assignments.insertOne(newAssignment);
-    return result;
+  create(newAssignment) {
+    return assignments.insertOne(newAssignment);
   },
 };
